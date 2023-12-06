2d550e191b66c8abcc177eb07e4c07341b771685
Venu Rani <venurani@Venus-MacBook-Air.local>
2023-12-06 17:19:39 +0530

    ver 0.2

--- app.py
+++ app.py
@@ -1,93 +1,109 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from autots import AutoTS
 import matplotlib.pyplot as plt
+import numpy as np
 
-def preprocess_data_for_customer_weekly(data, customer_name, start_date, end_date='2023-08-23'):
+# Function to preprocess data for a specific customer
+def preprocess_data_for_customer(data, customer_name, start_date, end_date, frequency):
     customer_data = data[data['Customer Name (Cleaned)'] == customer_name]
-    start_date = pd.to_datetime(start_date)
-    end_date = pd.to_datetime(end_date)
     customer_data = customer_data[(customer_data['Date'] >= start_date) & (customer_data['Date'] <= end_date)]
     customer_data = customer_data.groupby('Date').agg({'QTY': 'sum'}).reset_index()
-    return customer_data
+    aggregated_data = customer_data.set_index('Date').resample(frequency).sum().reset_index()
+    return aggregated_data
 
-def aggregate_weekly(preprocessed_data_weekly):
-    preprocessed_data_weekly = preprocessed_data_weekly.set_index('Date')
-    weekly_data = preprocessed_data_weekly.resample('W').sum().reset_index()
-    return weekly_data
-
-def impute_weekly_data(weekly_data, imputation_method):
-    weekly_data['QTY'] = weekly_data['QTY'].replace(0, np.nan)
+# Function to impute missing values
+def impute_data(aggregated_data, imputation_method):
+    aggregated_data['QTY'] = aggregated_data['QTY'].replace(0, np.nan)
     if imputation_method == 'ffill':
-        weekly_data['QTY'] = weekly_data['QTY'].ffill()
+        aggregated_data['QTY'] = aggregated_data['QTY'].fillna(method='ffill')
     elif imputation_method == 'bfill':
-        weekly_data['QTY'] = weekly_data['QTY'].bfill
+        aggregated_data['QTY'] = aggregated_data['QTY'].fillna(method='bfill')
     else:
-        weekly_data['QTY'] = weekly_data['QTY'].interpolate(method=imputation_method)
-    return weekly_data
+        aggregated_data['QTY'] = aggregated_data['QTY'].interpolate(method=imputation_method)
+    return aggregated_data
 
-# Streamlit UI setup
-st.title("Customer Sales Forecasting")
+# Function to plot data
+def plot_data(original_data, forecast_data, upper_confidence, lower_confidence):
+    plt.figure(figsize=(12, 6))
+    plt.plot(original_data['Date'], original_data['QTY'], label='Original Data', color='blue')
+    plt.plot(forecast_data['Forecast Interval'], forecast_data['Forecast Value'], label='Forecasted Data', color='green', linestyle='--')
+    plt.fill_between(forecast_data['Forecast Interval'], lower_confidence, upper_confidence, color='gray', alpha=0.3, label='Confidence Interval')
+    plt.title('Original vs Forecasted Data with Confidence Intervals')
+    plt.xlabel('Date')
+    plt.ylabel('QTY')
+    plt.legend()
+    plt.tight_layout()  # Adjust layout for better appearance
+    return plt
 
-uploaded_file = st.file_uploader("Upload your sales data file (Excel format)", type=['xlsx'])
+# Streamlit UI
+st.title("Sales Data Forecasting")
+
+st.sidebar.header("Input Options")
+
+# File upload
+uploaded_file = st.sidebar.file_uploader("Choose a file (Excel or CSV)", type=["xlsx", "csv"])
 if uploaded_file is not None:
-    data = pd.read_excel(uploaded_file)
+    # Read file
+    if uploaded_file.type == "text/csv":
+        data = pd.read_csv(uploaded_file)
+    else:
+        data = pd.read_excel(uploaded_file)
+
     data['Date'] = pd.to_datetime(data['Date'])
-    data = data[['Customer Name (Cleaned)', 'Date', 'QTY']]
 
-    customer_list = data['Customer Name (Cleaned)'].unique()
-    selected_customer = st.selectbox('Select a Customer', customer_list)
+    # Select Customer
+    customer_name = st.sidebar.selectbox("Select Customer", data['Customer Name (Cleaned)'].unique())
 
-    imputation_methods = ['ffill', 'bfill', 'akima', 'linear', 'cubic']
-    imputation_method = st.selectbox('Select an Imputation Method', imputation_methods)
+    # Select Aggregation Frequency
+    frequency = st.sidebar.selectbox("Select Aggregation Frequency", ['15D', 'W', 'M'])
 
-    confidence_interval = st.slider("Select Confidence Interval", 0.80, 0.99, 0.95)
+    # Select Imputation Method
+    imputation_method = st.sidebar.selectbox("Select Imputation Method", ['ffill', 'bfill', 'linear'])
 
-    if st.button('Forecast Sales'):
-        start_date = '2021-11-01'
-        preprocessed_data = preprocess_data_for_customer_weekly(data, selected_customer, start_date)
-        weekly_data = aggregate_weekly(preprocessed_data)
-        imputed_data = impute_weekly_data(weekly_data, imputation_method)
+    # Select Confidence Interval
+    confidence_interval = st.sidebar.slider("Select Confidence Interval", 0.0, 1.0, 0.9)
 
-        available_data_length = len(imputed_data)
-        forecast_length = int(len(imputed_data) * 0.2)  # Use 80% of data for training
+    if st.sidebar.button("Forecast"):
+        with st.spinner('Running the model...'):
+            start_date = data['Date'].min()
+            end_date = data['Date'].max()
 
-        model = AutoTS(forecast_length=forecast_length, frequency='W', prediction_interval=confidence_interval)
-        model = model.fit(imputed_data, date_col='Date', value_col='QTY')
+            # Data Preprocessing
+            preprocessed_data = preprocess_data_for_customer(data, customer_name, start_date, end_date, frequency)
+            imputed_data = impute_data(preprocessed_data, imputation_method)
 
-        prediction = model.predict(forecast_length=forecast_length)
-        forecast_df = prediction.forecast
+            # Initialize and fit the AutoTS model
+            model = AutoTS(
+                forecast_length=int(len(imputed_data) * 0.2),
+                frequency=frequency,
+                prediction_interval=confidence_interval,
+                ensemble='simple',
+                max_generations=5,
+                num_validations=2,
+                validation_method="backwards",
+            )
+            model = model.fit(imputed_data, date_col='Date', value_col='QTY', id_col=None)
 
-        # Extracting upper and lower confidence intervals
-        upper_confidence_interval = prediction.upper_forecast
-        lower_confidence_interval = prediction.lower_forecast
+            # Display chosen model
+            st.write("Chosen Model by AutoTS:")
+            st.text(str(model.best_model))
 
-        # Combine forecast with confidence intervals
-        forecast_combined = forecast_df.join(upper_confidence_interval, rsuffix='_upper').join(lower_confidence_interval, rsuffix='_lower')
+            # Generate predictions
+            prediction = model.predict()
+            forecast_df = prediction.forecast
 
-        # Display forecast data with confidence intervals
-        st.write("Forecasted Data with Confidence Intervals:")
-        st.dataframe(forecast_combined.head(forecast_length))
+            # Combine forecast and confidence intervals into one DataFrame
+            forecast_combined = forecast_df.copy()
+            forecast_combined['Forecast Interval'] = forecast_combined.index
+            forecast_combined['Lower Confidence Interval'] = prediction.lower_forecast['QTY']
+            forecast_combined['Upper Confidence Interval'] = prediction.upper_forecast['QTY']
+            forecast_combined.rename(columns={'QTY': 'Forecast Value'}, inplace=True)
+            
+            # Display forecast with intervals
+            st.write("Forecast with Confidence Intervals:")
+            st.dataframe(forecast_combined.reset_index(drop=True))
 
-        # Plotting
-        fig, ax = plt.subplots(figsize=(12, 6))
-        ax.plot(imputed_data['Date'], imputed_data['QTY'], label='Original Data')
-        ax.plot(forecast_df.index, forecast_df['QTY'], label='Forecasted Data', linestyle='--')
-        ax.fill_between(forecast_df.index, lower_confidence_interval['QTY'], upper_confidence_interval['QTY'], color='gray', alpha=0.3, label='Confidence Interval')
-        ax.legend()
-        ax.set_title('Original vs Forecasted Data (Weekly)')
-        ax.set_xlabel('Date')
-        ax.set_ylabel('QTY')
-        st.pyplot(fig)
-
-        # Display model details and error metrics
-        model_results = model.results()
-        best_model_results = model_results.iloc[0]  # Assuming the best model is at the top
-        st.write("Best Model Details:")
-        st.table(best_model_results[['Model', 'ModelParameters']])
-        st.write("Error Metrics for the Best Model:")
-        error_metrics_df = best_model_results.drop(['Model', 'ID', 'Generation', 'ModelParameters'])
-        st.table(error_metrics_df)
-
-# To run this script, save it as a Python file and execute 'streamlit run your_script.py'.
+            # Plotting
+            fig = plot_data(imputed_data, forecast_combined, forecast_combined['Lower Confidence Interval'], forecast_combined['Upper Confidence Interval'])
+            st.pyplot(fig)
