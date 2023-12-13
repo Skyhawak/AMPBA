fcf89fdb8de2323c3834246c8b7dfd5c6eb3e849
Venu Rani <venurani@Venus-MacBook-Air.local>
2023-12-13 22:50:58 +0530

    ver 2.9

--- app.py
+++ app.py
@@ -36,26 +36,43 @@ st.markdown("""
 st.sidebar.header("Input Options")
 
 # Standard dates for data processing
-standard_dates = [pd.to_datetime('2019-01-01'), pd.to_datetime('2021-01-01'), pd.to_datetime('2021-11-01')]
+standard_dates = {
+    '2019-01-01': pd.to_datetime('2019-01-01'),
+    '2021-01-01': pd.to_datetime('2021-01-01'),
+    '2021-11-01': pd.to_datetime('2021-11-01')
+}
 
-# Function to preprocess data for a specific customer
-def preprocess_data_for_customer(data, customer_name, frequency):
-    customer_data = data[data['Customer Name (Cleaned)'] == customer_name]
-    processed_data = pd.DataFrame()
-    for sd in standard_dates:
-        date_filtered_data = customer_data[customer_data['Date'] >= sd]
-        # Ensure only relevant columns are aggregated
-        cols_to_aggregate = date_filtered_data[['Date', 'QTY']]
-        aggregated_data = cols_to_aggregate.resample(frequency, on='Date').sum().reset_index()
-        processed_data = pd.concat([processed_data, aggregated_data])
-    return processed_data
+# Date selection
+selected_date = st.sidebar.selectbox("Select From Date", options=list(standard_dates.keys()))
 
-# Function to plot data
-def plot_combined_data(original_data, imputed_data):
+# File upload
+uploaded_file = st.sidebar.file_uploader("Choose a file (Excel or CSV)", type=["xlsx", "csv"])
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file) if uploaded_file.type == "text/csv" else pd.read_excel(uploaded_file)
+    data['Date'] = pd.to_datetime(data['Date'])
+
+    # Filter data from the selected start date to the last data point
+    filtered_data = data[data['Date'] >= standard_dates[selected_date]]
+
+    customer_name = st.sidebar.selectbox("Select Customer", filtered_data['Customer Name'].unique())
+    frequency = st.sidebar.selectbox("Select Aggregation Frequency", ['15D', 'W', 'M'])
+    confidence_interval = st.sidebar.slider("Select Confidence Interval", 0.80, 0.99, 0.95, 0.01)
+
+    # Aggregating data
+    aggregated_data = filtered_data.set_index('Date').resample(frequency).sum().reset_index()
+    aggregated_data_before_imputation = aggregated_data.copy()
+
+    # Imputation selection
+    imputation_methods = ['None', 'ffill', 'bfill', 'linear', 'akima', 'cubic']
+    imputation_method = st.sidebar.selectbox("Select Imputation Method", imputation_methods)
+    if imputation_method != 'None':
+        aggregated_data['QTY'] = aggregated_data['QTY'].replace(0, np.nan).interpolate(method=imputation_method)
+
+    # Plot combined data
+    st.subheader("Data Over Time Before and After Imputation")
     plt.figure(figsize=(12, 6))
-    plt.plot(original_data['Date'], original_data['QTY'], label='Data Before Imputation', color='blue')
-    if imputed_data is not None:
-        plt.plot(imputed_data['Date'], imputed_data['QTY'], label='Data After Imputation', color='red', linestyle='--')
+    plt.plot(aggregated_data_before_imputation['Date'], aggregated_data_before_imputation['QTY'], label='Data Before Imputation', color='blue')
+    plt.plot(aggregated_data['Date'], aggregated_data['QTY'], label='Data After Imputation', color='red', linestyle='--')
     plt.title('Data Over Time Before and After Imputation')
     plt.xlabel('Date')
     plt.ylabel('QTY')
@@ -63,37 +80,11 @@ def plot_combined_data(original_data, imputed_data):
     plt.tight_layout()
     st.pyplot(plt)
 
-# File upload
-uploaded_file = st.sidebar.file_uploader("Choose a file (Excel or CSV)", type=["xlsx", "csv"])
-if uploaded_file is not None:
-    data = pd.read_excel(uploaded_file)
-    data['Date'] = pd.to_datetime(data['Date'])
-
-    customer_name = st.sidebar.selectbox("Select Customer", data['Customer Name (Cleaned)'].unique())
-    frequency = st.sidebar.selectbox("Select Aggregation Frequency", ['15D', 'W', 'M'])
-    confidence_interval = st.sidebar.slider("Select Confidence Interval", 0.80, 0.99, 0.95, 0.01)
-
-    preprocessed_data = preprocess_data_for_customer(data, customer_name, frequency)
-    
-    # Imputation selection
-    imputation_methods = ['None', 'ffill', 'bfill', 'linear', 'akima', 'cubic']
-    imputation_method = st.sidebar.selectbox("Select Imputation Method", imputation_methods)
-    imputed_data = None
-    if imputation_method != 'None':
-        imputed_data = preprocessed_data.copy()
-        imputed_data['QTY'] = imputed_data['QTY'].replace(0, np.nan)
-        if imputation_method in imputation_methods[1:]:  # Check if the method is in the list
-            imputed_data['QTY'] = imputed_data['QTY'].interpolate(method=imputation_method)
-
-    # Plot combined data
-    st.subheader("Data Over Time Before and After Imputation")
-    plot_combined_data(preprocessed_data, imputed_data)
-
-    # Forecasting and plotting
+    # Forecasting and plotting if the forecast button is clicked
     if st.sidebar.button("Forecast"):
         with st.spinner('Running the model...'):
             model = AutoTS(
-                forecast_length=int(len(imputed_data) * 0.2),
+                forecast_length=int(len(aggregated_data) * 0.2),
                 frequency=frequency,
                 prediction_interval=confidence_interval,
                 ensemble='simple',
@@ -101,11 +92,10 @@ if uploaded_file is not None:
                 num_validations=2,
                 validation_method="backwards",
             )
-            model = model.fit(imputed_data, date_col='Date', value_col='QTY', id_col=None)
+            model = model.fit(aggregated_data, date_col='Date', value_col='QTY', id_col=None)
 
             st.write("Chosen Model by AutoTS:")
             try:
-                # Retrieve and display the best model's summary
                 best_model_summary = model.best_model['Model Summary']
                 st.text(best_model_summary)
             except KeyError as e:
@@ -122,15 +112,3 @@ if uploaded_file is not None:
 
             st.write("Forecast with Confidence Intervals:")
             st.dataframe(forecast_combined.reset_index(drop=True))
-
-            # Plotting the forecast
-            plt.figure(figsize=(12, 6))
-            plt.plot(imputed_data['Date'], imputed_data['QTY'], label='Historical Data', color='blue')
-            plt.plot(forecast_combined['Forecast Interval'], forecast_combined['Forecast Value'], label='Forecasted Data', color='green', linestyle='--')
-            plt.fill_between(forecast_combined['Forecast Interval'], forecast_combined['Lower Confidence Interval'], forecast_combined['Upper Confidence Interval'], color='gray', alpha=0.3, label='Confidence Interval')
-            plt.title('Historical vs Forecasted Data with Confidence Intervals')
-            plt.xlabel('Date')
-    plt.ylabel('QTY')
-    plt.legend()
-    plt.tight_layout()
-    st.pyplot(plt)
